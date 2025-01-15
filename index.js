febf20d2e08f0c8129deaa7588ee0b12937be70d
LuisF973 <luisfelipe.k.m.rodrigues@gmail.com>
2025-01-15 17:23:47 -0300

    luis

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ const { Curso } = require("./src/models/Curso");
 
 const alunos = new AlunoController()
 
+alunos.adicionarAluno('fulano', 'fulano@gmail.com', '84999998', '1234567', 'tec em desenvolvimento')
 alunos.listarAluno();
